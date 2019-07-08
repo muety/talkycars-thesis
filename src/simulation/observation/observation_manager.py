6a148ff8858a982ba0ccc273a73a353cc563c220
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-07-08 07:46:57 +0200

    Remove locks - did not make sense.

--- src/simulation/observation/observation_manager.py
+++ src/simulation/observation/observation_manager.py
@@ -1,5 +1,4 @@
 from collections import deque
-from threading import Lock
 from typing import Callable
 
 from observation.observation import Observation
@@ -10,7 +9,6 @@ class ObservationManager:
         self.observations = {}
         self.key_types = {}
         self.subscribers = {}
-        self.locks = {}
 
     def register_key(self, key, obs_type, keep=10):
         assert isinstance(key, str)
@@ -18,7 +16,6 @@ class ObservationManager:
 
         self.observations[key] = deque(maxlen=keep)
         self.key_types[key] = obs_type
-        self.locks[key] = Lock()
 
     def subscribe(self, key, callable: Callable):
         if not key in self.subscribers:
@@ -33,19 +30,11 @@ class ObservationManager:
         assert isinstance(key, str)
         assert isinstance(observation, Observation)
 
-        lock = self.locks[key]
-        if lock.locked():
-            return
-
-        lock.acquire()
-
         self.observations[key].append(observation)
 
         if key in self.subscribers:
             for f in self.subscribers[key]:
                 f(observation)
-
-        lock.release()
 
     def latest(self, key):
         return self.observations[key][-1]
