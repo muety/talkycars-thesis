6f6c8b0705ef4f7829d9de99b07cb669424e32af
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-07-11 19:06:38 +0200

    Locks again.

--- src/simulation/observation/observation_manager.py
+++ src/simulation/observation/observation_manager.py
@@ -1,5 +1,5 @@
 from collections import deque
-from threading import Thread
+from threading import Thread, Lock
 from typing import Callable
 
 from observation import Observation
@@ -10,6 +10,7 @@ class ObservationManager:
         self.observations = {}
         self.key_types = {}
         self.subscribers = {}
+        self.locks = {}
 
     def register_key(self, key, obs_type, keep=10):
         assert isinstance(key, str)
@@ -17,6 +18,7 @@ class ObservationManager:
 
         self.observations[key] = deque(maxlen=keep)
         self.key_types[key] = obs_type
+        self.locks[key] = Lock()
 
     def subscribe(self, key, callable: Callable):
         if not key in self.subscribers:
@@ -31,12 +33,25 @@ class ObservationManager:
         assert isinstance(key, str)
         assert isinstance(observation, Observation)
 
+        lock = self.locks[key]
+        if lock.locked():
+            return
+
+        lock.acquire()
+
         self.observations[key].append(observation)
 
+        threads = []
         if key in self.subscribers:
             for f in self.subscribers[key]:
-                t = Thread(target=lambda o: f(o), args=(observation,))
+                t = Thread(target=f, args=(observation,))
                 t.start()
+                threads.append(t)
+
+        for t in threads:
+            t.join()
+
+        lock.release()
 
     def latest(self, key):
         if not key in self.observations or len(self.observations[key]) == 0:
