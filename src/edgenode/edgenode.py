d107242551360be2058f47c0d07784d76afeafd1
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-07-24 22:41:03 +0200

    Compute edge node's message rate.

--- src/edgenode/edgenode.py
+++ src/edgenode/edgenode.py
@@ -1,4 +1,7 @@
 import logging
+import sys
+import time
+from threading import RLock, Thread
 from typing import Type, cast, Iterable, Set
 
 from common.bridge import MqttBridge
@@ -7,6 +10,7 @@ from common.serialization.schema import CapnpObject
 from common.serialization.schema.base import PEMTrafficScene
 from edgenode.fusion import FusionService
 
+EVAL_RATE_SECS = 5
 
 class EdgeNode:
     def __init__(self):
@@ -14,7 +18,13 @@ class EdgeNode:
         self.fusion_srvc: FusionService = FusionService()
         self.covered_quadkeys: Set[str] = set()  # TODO: Read from env
 
+        self.rate_count_thread: Thread = Thread(target=self._eval_rate)
+        self.rate_lock: RLock = RLock()
+        self.rate_count: int = -1
+
     def run(self):
+        self.rate_count_thread.start()
+
         self.mqtt = MqttBridge()
         self.mqtt.subscribe(TOPIC_PREFIX_GRAPH_RAW_IN + '/#', self._on_graph)
         self.mqtt.listen()
@@ -27,8 +37,23 @@ class EdgeNode:
 
         covered_keys: Iterable[str] = self.covered_quadkeys if len(self.covered_quadkeys) > 0 else fused_graph.occupancy_grid.get_parent_tiles(REMOTE_GRID_TILE_LEVEL)
         encoded_graph: bytes = fused_graph.to_bytes()
+
         for k in covered_keys:
             self.mqtt.publish(f'{TOPIC_PREFIX_GRAPH_FUSED_OUT}/{k}', encoded_graph)
+
+        with self.rate_lock:
+            self.rate_count += 1
+
+    def _eval_rate(self):
+        while not self.mqtt or not self.mqtt.connected:
+            time.sleep(.5)
+
+        while self.mqtt.connected:
+            time.sleep(EVAL_RATE_SECS)
+
+            with self.rate_lock:
+                logging.debug(f'Current message rate: {self.rate_count / EVAL_RATE_SECS} msgs / sec')
+                self.rate_count = 0
 
     def _decode_capnp_msg(self, bytes: bytes, target_cls: Type[CapnpObject]) -> CapnpObject:
         try:
@@ -39,6 +64,8 @@ class EdgeNode:
             logging.error(e2)
             raise e2
 
-def run():
-    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+def run(args=sys.argv[1:]):
+    level = logging.DEBUG if '--verbose' in args else logging.INFO
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=level)
     EdgeNode().run()
