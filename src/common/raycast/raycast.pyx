5429912a3312f6fca5572c1be0aa507507c5bef8
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-09-03 12:21:38 +0200

    Set Cython flags globally.

--- src/common/raycast/raycast.pyx
+++ src/common/raycast/raycast.pyx
@@ -1,25 +1,21 @@
-# distutils: extra_compile_args=-fopenmp
-# distutils: extra_link_args=-fopenmp
+#!python
+#cython: boundscheck=False
+#cython: wraparound=False
+#cython: nonecheck=False
+#cython: cdivision=True
 
 cimport numpy as np
-cimport cython
 import numpy
 
 cdef class Ray3D:
     cdef public const np.float32_t[:] origin
     cdef public const np.float32_t[:] direction
 
-    @cython.boundscheck(False)
-    @cython.wraparound(False)
-    @cython.nonecheck(False)
     def __cinit__(self, const np.float32_t[:] origin, const np.float32_t[:] direction):
         self.origin = origin
         self.direction = direction
 
 # https://gamedev.stackexchange.com/a/103714
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
 cpdef bint aabb_intersect(const np.float32_t[:,:] bounds, Ray3D ray):
     cdef np.float32_t[:] t = numpy.zeros((9,)).astype(numpy.float32)
     cdef const np.float32_t[:] vmin, vmax
@@ -37,9 +33,6 @@ cpdef bint aabb_intersect(const np.float32_t[:,:] bounds, Ray3D ray):
     t[8] = min(min(max(t[1], t[2]), max(t[3], t[4])), max(t[5], t[6]))
     return not (t[8] < 0 or t[7] > t[8])
 
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.nonecheck(False)
 cpdef bint aabb_contains(const np.float32_t[:,:] bounds, const np.float32_t[:] point):
     return all([
         bounds[0][0] <= point[0] <= bounds[1][0],
