15bfc2c721090bdf96755490aba9e8c847d06987
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-10-15 14:32:38 +0200

    Partial flushes.

--- src/evaluation/perception/grid_collector.py
+++ src/evaluation/perception/grid_collector.py
@@ -1,30 +1,31 @@
 import argparse
-import datetime
 import logging
 import pickle
 import sys
 import time
+from datetime import datetime
+from threading import Thread
 from typing import List
 
 import carla
 from common.bridge import MqttBridge
 from common.constants import *
 from common.quadkey import QuadKey
-from common.serialization.schema.base import PEMTrafficScene
-from common.serialization.schema.occupancy import PEMOccupancyGrid
 from common.util import GracefulKiller
 
 BASE_KEY = '120203233231202'  # Town01
 DATA_DIR = '../../../data/evaluation/perception'
+FLUSH_AFTER = 1e2
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
 
-class PEMGridContainer:
-    def __init__(self, grid: PEMOccupancyGrid, tile: QuadKey):
-        self.grid = grid
+class ObservationMessageContainer:
+    def __init__(self, msg: bytes, tile: QuadKey):
+        self.msg = msg
         self.tile = tile
         self.ts = time.time()
+
 
 class GridCollector:
     def __init__(
@@ -46,11 +47,13 @@ class GridCollector:
         self.bridge: MqttBridge = MqttBridge()
         self.client: carla.Client = carla_client
 
+        self.start_time: datetime = datetime.now()
         self.last_tick: float = 0
         self.tick_count: int = 0
+        self.flush_count: int = 0
 
-        self.observation_buffer: List[PEMGridContainer] = []
-        self.occupancy_observations: List[PEMGridContainer] = []
+        self.observation_buffer: List[ObservationMessageContainer] = []
+        self.occupancy_observations: List[ObservationMessageContainer] = []
 
         for d in [self.data_dir, self.data_dir_observed, self.data_dir_actual]:
             if not os.path.exists(d):
@@ -80,24 +83,31 @@ class GridCollector:
     def tick(self):
         # Flush observation buffer
         self.occupancy_observations += self.observation_buffer[:]
-        self.observation_buffer = []
+        self.observation_buffer.clear()
 
         if self.tick_count % 10 == 0:
-            logging.debug(f'{len(self.occupancy_observations)}')
+            k = len(self.occupancy_observations)
+            n = k + (FLUSH_AFTER * self.flush_count)
+
+            logging.debug(f'Observations {k} (~ {n})')
+
+            if k > FLUSH_AFTER and k % (FLUSH_AFTER * (self.flush_count + 1)):
+                Thread(target=self.flush).start()
 
         self.tick_count += 1
 
     def flush(self):
-        dt = datetime.datetime.now()
-        tpl = f'{self.base_tile.key}_%Y-%m-%d_%H-%M-%S.pkl'
+        tpl = f'{self.base_tile.key}_%Y-%m-%d_%H-%M-%S_part-{self.flush_count + 1}.pkl'
 
         logging.info('Flushing observations ...')
-        with open(os.path.join(self.data_dir_observed, dt.strftime(tpl)), 'wb') as f:
+        with open(os.path.join(self.data_dir_observed, self.start_time.strftime(tpl)), 'wb') as f:
             pickle.dump(self.occupancy_observations, f)
 
+        self.occupancy_observations.clear()
+        self.flush_count += 1
+
     def on_remote_grid(self, tile: QuadKey, msg: bytes):
-        scene: PEMTrafficScene = PEMTrafficScene.from_bytes(msg)
-        self.observation_buffer.append(PEMGridContainer(scene.occupancy_grid, tile))
+        self.observation_buffer.append(ObservationMessageContainer(msg, tile))
 
 
 def run(args=sys.argv[1:]):
