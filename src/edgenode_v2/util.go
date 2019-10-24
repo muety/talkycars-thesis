009d5008aac67406b6e5a4e68fbc06b2032f3220
Ferdinand Mütsch <mail@ferdinand-muetsch.de>
2019-10-24 13:04:34 +0200

    Add quadint cache.

--- src/edgenode_v2/util.go
+++ src/edgenode_v2/util.go
@@ -1,24 +1,41 @@
 package main
 
 import (
-	"strconv"
 	"bytes"
+	"strconv"
+	"sync"
+)
+
+var (
+	qi2qkCache sync.Map = sync.Map{}
+	qk2qiCache sync.Map = sync.Map{}
 )
 
 func quadInt2QuadKey(quadint uint64) string {
+	if qk, ok := qi2qkCache.Load(quadint); ok {
+		return qk.(string)
+	}
+
 	var buffer bytes.Buffer
 	zoom := int(quadint & 0b11111)
-	
+
 	for i := 0; i < zoom; i++ {
 		bit_loc := (64 - ((i + 1) * 2))
 		char_bits := ((quadint & (0b11 << bit_loc)) >> bit_loc)
 		buffer.WriteString(strconv.Itoa(int(char_bits)))
 	}
 
-	return buffer.String()
+	qk := buffer.String()
+	qi2qkCache.Store(quadint, qk)
+
+	return qk
 }
 
 func quadKey2QuadInt(quadkey string) (uint64, error) {
+	if qi, ok := qk2qiCache.Load(quadkey); ok {
+		return qi.(uint64), nil
+	}
+
 	var qi uint64
 	zoom := len(quadkey)
 
@@ -31,6 +48,8 @@ func quadKey2QuadInt(quadkey string) (uint64, error) {
 		qi |= uint64(char) << bit_loc
 	}
 	qi |= uint64(zoom)
+
+	qk2qiCache.Store(quadkey, qi)
 
 	return qi, nil
 }
